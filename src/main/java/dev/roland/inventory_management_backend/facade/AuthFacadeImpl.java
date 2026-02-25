8950f3dd70068b70f1a4bab5000debd1bfdbbd5e
Roland Szabo <szabo.roland.1213@gmail.com>
2026-02-25 22:50:19 +0100

    [IM-30] Fix token storage and authguards - Backend
    - Added spotless to the project and applied it

--- src/main/java/dev/roland/inventory_management_backend/facade/AuthFacadeImpl.java
+++ src/main/java/dev/roland/inventory_management_backend/facade/AuthFacadeImpl.java
@@ -152,6 +152,7 @@ public class AuthFacadeImpl implements AuthFacade {
         loginSessionService.createTemporarySessionWithExpiry(user.getEmail());
 
     return ShortLifeTokenResponse.builder()
+        .is2faEnabled(user.is2faEnabled())
         .shortLifeToken(tokenWithExpiry.getToken())
         .expiresAt(tokenWithExpiry.getExpiresAt())
         .build();
